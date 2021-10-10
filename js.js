89aa77398c93a8e947c05881f935a8fb8ed6f560
Keshav-Lolljee <92163312+Keshav-Lolljee@users.noreply.github.com>
2021-10-10 19:57:19 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,47 @@
+
+
+var checkCollapse1 = true;
+
+function collapseBtn1() {
+    if (checkCollapse1) {
+        document.getElementById("collapse-Btn1").classList.remove("inverseCollapse");
+        document.getElementById("collapse-Btn1").classList.remove("resetCollapse");
+        document.getElementById("inner-list1").classList.remove("hideList");
+        document.getElementById("inner-list1").classList.remove("showList");
+
+        document.getElementById("collapse-Btn1").classList.add("inverseCollapse");
+        document.getElementById("inner-list1").classList.add("showList");
+
+        checkCollapse1 = false;
+    }
+    else if (!checkCollapse1) {
+        document.getElementById("collapse-Btn1").classList.remove("inverseCollapse");
+        document.getElementById("collapse-Btn1").classList.remove("resetCollapse");
+        document.getElementById("inner-list1").classList.remove("hideList");
+        document.getElementById("inner-list1").classList.remove("showList");
+
+        document.getElementById("collapse-Btn1").classList.add("resetCollapse");
+        document.getElementById("inner-list1").classList.add("hideList");
+
+        checkCollapse1 = true;
+    }
+    
+}
+
+var checkCollapse2 = true;
+
+function collapseBtn2() {
+    if (checkCollapse2) {
+        document.getElementById("collapse-Btn2").classList.remove("inverseCollapse");
+        document.getElementById("collapse-Btn2").classList.remove("resetCollapse");
+        document.getElementById("collapse-Btn2").classList.add("inverseCollapse");
+        checkCollapse2 = false;
+    }
+    else if (!checkCollapse2) {
+        document.getElementById("collapse-Btn2").classList.remove("inverseCollapse");
+        document.getElementById("collapse-Btn2").classList.remove("resetCollapse");
+        document.getElementById("collapse-Btn2").classList.add("resetCollapse");
+        checkCollapse2 = true;
+    }
+    
+}
